e7d88af94050794ed23ee2bc801075cc7df3561d
yoshi <mdmdmdmdmd57@gmail.com>
2023-02-17 22:45:04 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => 'articles#index'
   devise_for :users
   resources :articles
+  resources :mypage
   resources :sample_articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
