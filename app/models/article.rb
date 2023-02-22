27a48783c792f51da3a1796781a9ae9f2b42bc00
yoshi <mdmdmdmdmd57@gmail.com>
2023-02-22 16:29:00 +0900

    has_manyなど関係性の追記、記載ミスの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
+  has_many :article_tags
+  has_many :tags, :through => :article_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_135716) do
+ActiveRecord::Schema.define(version: 2023_02_22_070647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_tags", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_article_tags_on_article_id"
+    t.index ["tag_id"], name: "index_article_tags_on_tag_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 2023_02_21_135716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "article_tags", "articles"
+  add_foreign_key "article_tags", "tags"
   add_foreign_key "articles", "users"
 end
